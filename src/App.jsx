8b038a2e6085f1f8eae7d14b6640de50d5df01ac
Savraj <jarvashgnis12@gmail.com>
2022-11-14 15:26:10 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+    };
+  }
+
+  clickHandler = () => {
+
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <button> click </button>
+      </div>
+    );
+  }
+}
+
+export default App;
